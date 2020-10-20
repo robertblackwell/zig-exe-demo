4adf1411ae556cfcec702eabe5f71881cbac45a7
robert_blackwell <rob@whiteacorn.com>
2020-10-19 19:20:48 -0700

    tidied up names and separated the zig and c ascii libs

--- src/lib/record.zig
+++ src/lib/record.zig
@@ -1,30 +1,55 @@
 const std = @import("std");
 const z_ascii = @import("z_ascii.zig");
 
+const address_length = 100;
+
+fn mkname(aname: []const u8) []u8 {
+    var tmp: []u8 = std.heap.c_allocator.alloc(u8, aname.len) catch unreachable;
+    std.mem.copy(u8, tmp, aname);
+    return tmp;
+}
+
 pub const Record = struct {
-    name: [100]u8,
-    pub fn init(aname: []const u8) Record {
-        var r = Record {
+    name: []const u8,
+    address: [100]u8,
+
+    pub fn init(aname: []const u8, an_addr:[]const u8) Record {
+        var tmp: []u8 = std.heap.c_allocator.alloc(u8, aname.len) catch unreachable;
+        std.mem.copy(u8, tmp, aname);
+
+        var r = Record{
+            .name = tmp,
+            .address = [_]u8{0} ** address_length,
         };
-        var i = 0;
-        while (i < aname.len) {
-            r.name[i] = aname[i];
-            i = i + 1;
-        }
+        std.mem.copy(u8, &(r.address), an_addr);
+        return r;
     }
-    pub fn set_name(self: Record, name: []const u8) viod {
-        self.name = name;
+    pub fn set_name(self: *Record, aname: []const u8) void {
+        // std.heap.c_allocator.free(self.name);
+        var tmp = std.heap.c_allocator.alloc(u8, aname.len) catch unreachable;
+        var tmp_slice = tmp[0..aname.len];
+        std.debug.warn("{}\n", .{@typeName(@TypeOf(tmp))});
+        std.debug.warn("{}\n", .{@typeName(@TypeOf(tmp_slice))});
+        std.mem.copy(u8, tmp, aname);
+        // // self.name = mkname(aname);
+        self.name = tmp[0..aname.len];
     } 
-    pub fn get_name() []u8 {
+    pub fn get_name(self: *Record) []const u8 {
         return self.name;
+    }
+    pub fn set_address(self: *Record, addr: []const u8) void {
+        std.mem.copy(u8, self.address, addr);
+    }
+    pub fn get_address(self: *Record) []const u8 {
+        return self.address;
     }
     pub fn toupper_name(self: Record) void {
         const bufsize = 100;
         const buf_len = bufsize;
         var out_str = [_]u8{0} ** bufsize;
         var slice = out_str[0..bufsize];
-
-        // var res = z_ascii.z_toupper_inplace(self.name);
+        // var aslice: []u8 = self.name[0..100];
+        // z_ascii.z_toupper_inplace(aslice);
         std.debug.warn("Here\n", .{});
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -71,9 +71,53 @@ pub fn demo_c_toupper_with_error_checking(comptime bufsize: usize) void {
         var result = out_str[0..@intCast(usize, uc_len)];
     }
 }
+/// copy a slice into a buffer and resturn a slice of that buffer representing the copied data
+///
+/// @param dst a slice of a destination buffer - it sets the max value on the size of the copy
+/// @param src a const slice the source from which the copy will come
+/// @return a sub slice of dst representing the data that was copied
+///
+fn z_cpy(dst: []u8, src: []const u8) []u8 {
+    var l1 = dst.len;
+    for(src) |ch, i| {
+        dst[i] = src[i];
+    }
+    return dst[0..src.len];
+}
+fn demo_string_copy() void {
+    var s = "thisisastring";
+    var buf:[100]u8 = [_]u8{0} ** 100;
+    for (s) |ch, i| {
+        buf[i] = s[i];
+    }
+    var slice1 = buf[0..s.len];
 
-pub fn main() void 
+    for(s) |ch, i| {
+        assert(s[i] == buf[i]);
+    }
+    var buf2:[100]u8 = [_]u8{0} ** 100;
+    var slice2 = buf2[0..100];
+    var res_slice = z_cpy(slice2, slice1);
+    std.debug.warn("Hello \n", .{});
+}
+fn record_set_name(record: *Record, aname: []const u8) void {
+    var allocd_slice = std.heap.c_allocator.alloc(u8, 100) catch unreachable;
+    std.mem.copy(u8, allocd_slice, aname);
+    var new_slice = allocd_slice[0..aname.len];
+    var slx  = allocd_slice;
+    slx = new_slice;
+    record.name = slx;
+    std.debug.warn("Done\n", .{});
+}
+pub fn main() !void 
 {
+    // set up an allocator
+    const allocator = std.heap.c_allocator;
+
+    var x:[]u8 = try allocator.alloc(u8, 21);
+    var y = try allocator.alloc(u8, 9);
+    var z = try allocator.alloc(u8, 1);
+
     // note putting the c_functions_version() call in the print statement like so
     // std.debug.warn("main - using c_functions version {}\n", .{c_functions_version()}")
     // prints the pointer value not the string value
@@ -84,10 +128,21 @@ pub fn main() void
     demo_zig_toupper_with_error_checking(10);
     demo_c_toupper_with_error_checking(100);
     demo_c_toupper_with_error_checking(10);
+    demo_string_copy();
 
-    var tst = Record.init("hello this is the world");
+    var tst = Record.init("hello this is the world", "Andthisisanaddress");
     std.debug.warn("tst.name is : {}\n", .{tst.name});
-    tst.toupper_name();
+    var xname: []const u8 = "Yetanothername";
+    var allocd_slice = std.heap.c_allocator.alloc(u8, 100) catch unreachable;
+    std.mem.copy(u8, allocd_slice, xname);
+    var new_slice = allocd_slice[0..xname.len];
+    var slx  = allocd_slice;
+    slx = new_slice;
+    tst.name = slx;
+    var nn = @typeName(@TypeOf(tst.name));
+    record_set_name(&tst, "Someonelsesnamehere" );
+    var wn = tst.get_name();
+    tst.set_name("AnewName");
     std.debug.warn("tst.name is : {}\n", .{tst.name});
 
 }
